179ec5c78cb7dd7a5911a4915606143c0a0ac712
kubil <bilkisismail07@gmail.com>
2026-02-28 14:29:54 +0700

    initialization

--- src/app/(private)/trading-plans/loading.tsx
+++
@@ -1,7 +0,0 @@
-import View from "./view";
-
-function Loading() {
-  return <View loading />;
-}
-
-export default Loading;
--- src/app/(private)/trading-setups/loading.tsx
+++
@@ -1,7 +0,0 @@
-import View from "./view";
-
-function Loading() {
-  return <View loading list={{ data: [], status: false }} />;
-}
-
-export default Loading;
